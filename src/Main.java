3fa9aada244c05671276d302db49126170d61c0b
Marko Nikic <darkblue.azurite@gmail.com>
2018-03-14 13:16:13 +0100

    create aw junit template

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void read_and_solve(InputStream in, PrintStream out) {
+        Scanner s = new Scanner(in);
+    }
+
+    public static void main(String[] args) {
+        read_and_solve(System.in, System.out);
+    }
+}
